63797c0b1a5a0501f9a897e48efec1d79e6c2b96
IliHanSoLow <ilian-ode@gmx.de>
2024-05-07 22:02:06 +0200

    removed build from flake

--- flake.nix
+++ flake.nix
@@ -19,17 +19,6 @@
               ninja
             ];
           };
-          packages.x86_64-linux.c_http = pkgs.stdenv.mkDerivation {
-            name = "c_http";
-            src = ./src/main.c;
-            buildPhase = "make";
-            preBuild = "make setup";
-            installPhase = ''
-              mkdir $out/bin
-              cp build/c_http $out/bin/
-            '';
-          };
-          defaultPackage.x86_64-linux = self.packages.x86_64-linux.c_http;
         }
     );
 }
