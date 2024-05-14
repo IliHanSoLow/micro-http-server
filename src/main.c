ec4e65a5ae46a2c3f503620e76891eb391ba667a
IliHanSoLow <ilian-ode@gmx.de>
2024-05-14 19:14:23 +0200

    changes

--- src/main.c
+++ src/main.c
@@ -1,1 +1,20 @@
-int main() { return 0; }
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+int main() {
+  int tcp_socket;
+  struct sockaddr_in server_address;
+  if ((tcp_socket = socket(AF_INET, SOCK_STREAM, 0) < 0)) {
+    printf("socket failed");
+    exit(EXIT_FAILURE);
+  }
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = INADDR_ANY;
+
+  return 0;
+}
+
+int test() {}
